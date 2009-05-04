6ee02b1a53bf73ec10811fd5d3b143f500d26796
Sergio Pascual <sergiopr@fis.ucm.es>
2009-05-04 14:29:41 +0000

    Moving to 0.2.1

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ ext2 = Extension('milia.lumfuncs', ['src/lumfuncs_wrap.cc'],
   libraries = ['boost_python', 'milia', 'gsl', 'gslcblas'])
 
 setup(name='pymilia',
-      version='0.2.0',
+      version='0.2.1',
       author='Sergio Pascual',
-      author_email='spr@astrax.fis.ucm.es',
+      author_email='sergiopr@astrax.fis.ucm.es',
       url='https:// halmax.fis.ucm.es/projects/milia/wiki',
       license='GPLv3',
       description='Cosmological distances and ages',
