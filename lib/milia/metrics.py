d4853c4b86ce806f04c4ae6ec6e5b3811195d117
Sergio Pascual <sergiopr@fis.ucm.es>
2013-03-27 01:10:33 +0100

    Deprecation warning in metrics module

---
+++ lib/milia/metrics.py
@@ -0,0 +1,28 @@
+#
+# Copyright 2009-2013 Sergio Pascual
+# 
+# This file is part of PyMilia
+# 
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PyMilia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PyMilia.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+'''Cosmological metrics'''
+
+import warnings
+warnings.warn("the milia.metrics module is deprecated, use milia instead", 
+        DeprecationWarning, stacklevel=2)
+
+from ._milia import FlrwNat
+from ._milia import Flrw
+
+Metric = Flrw
