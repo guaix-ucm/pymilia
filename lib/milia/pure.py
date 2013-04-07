c71441c94ebf994e69d59618bc916b4dc008e1bf
Sergio Pascual <sergiopr@fis.ucm.es>
2013-04-08 00:14:38 +0200

    Creating empty pure python implementation

---
+++ lib/milia/pure.py
@@ -0,0 +1,19 @@
+#
+# Copyright 2009-2013 Sergio Pascual
+# 
+# This file is part of PyMilia
+# 
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PyMilia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PyMilia.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+'''Pure Python implementation of Flrw and FlrwNat'''
